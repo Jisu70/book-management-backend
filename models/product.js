bb54d2d6e35d5252db13b891748f9306b8c94284
Sudipta jana <sudiptajana70@gmail.com>
2023-06-12 21:21:05 +0530

    fixed database define model

--- models/product.js
+++ models/product.js
@@ -1,32 +1,32 @@
-  const Sequalize  = require('sequelize') ;
+const Sequalize  = require('sequelize') ;
 
-  const sequalize = require('../util/database')
+const sequalize = require('../util/database')
 
 
 
-  const Product = sequalize.define('product', {
-    id : {
-      type :Sequalize.INTEGER,
-      autoIncrement : true,
-      allowNull: false,
-      primaryKey : true 
-    },
-    title : {
-      type :Sequalize.STRING,
-      price : {
-        type : Sequalize.DOUBLE,
-        allowNull : false
-      } 
-    },
-    imageUrl : {
-      type :Sequalize.STRING,
-      allowNull: false,
-    },
-    description : {
-      type :Sequalize.STRING,
-      allowNull: false,
-    },
+const Product = sequalize.define('product', {
+  id : {
+    type :Sequalize.INTEGER,
+    autoIncrement : true,
+    allowNull: false,
+    primaryKey : true 
+  },
+  title : {
+    type :Sequalize.STRING,
+  },
+  price : {
+    type : Sequalize.DOUBLE,
+    allowNull : false
+  } ,
+  imageUrl : {
+    type :Sequalize.STRING,
+    allowNull: false,
+  },
+  description : {
+    type :Sequalize.STRING,
+    allowNull: false,
+  },
 
-  })
+})
 
-  module.exports = Product
+module.exports = Product
